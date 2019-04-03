b600336fb79184338f5e4f3b7accffade3426ab5
Chun Tak Li <lichuntak@hotmail.com>
2019-04-03 21:07:51 +0200

    Added unit tests

--- GYG_TaskTests/GYG_TaskTests.swift
+++ GYG_TaskTests/GYG_TaskTests.swift
@@ -10,7 +10,8 @@ import XCTest
 @testable import GYG_Task
 
 class GYG_TaskTests: XCTestCase {
-
+    let TEST_TIME_OUT_INTERVAL = 5.0
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,9 +20,32 @@ class GYG_TaskTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // Test Model
+    
+    func testReview() {
+        let review = Review()
+        XCTAssertEqual(review.reviewId, -1, "review id should be '-1'")
+        XCTAssertEqual(review.rating, "", "review rating should be ''")
+        XCTAssertEqual(review.title, "", "review title should be ''")
+        XCTAssertEqual(review.message, "", "review message should be ''")
+        XCTAssertEqual(review.author, "", "review author should be ''")
+        XCTAssertEqual(review.date, "", "review date should be ''")
+    }
+
+    // Test DataManager
+    
+    func testGetReviews() {
+        let expectation = self.expectation(description: "getReviewsExpectation")
+        DataManager.sharedInstance.getReviews(callback: { (total: Int, reviews: [Review]?, error: Error?) in
+            XCTAssertNil(error, "Retuen error from getReviews")
+            if let reviews = reviews {
+                XCTAssertGreaterThan(reviews.count, 0, "Should return some reviews from server")
+                expectation.fulfill()
+            }
+        })
+        self.waitForExpectations(timeout: TEST_TIME_OUT_INTERVAL) { (error: Error?) in
+            XCTAssertNil(error, "Retuen error from waitForExpectationsWithTimeout")
+        }
     }
 
     func testPerformanceExample() {
